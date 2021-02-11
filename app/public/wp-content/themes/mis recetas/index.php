f0770279b93877fe7ae25d8df5feb49b5de6fe54
FerLema18 <77021266+FerLema18@users.noreply.github.com>
2021-02-11 19:15:58 +0100

    archivos iniciales de wordpress

---
+++ app/public/wp-content/themes/mis recetas/index.php
@@ -0,0 +1,14 @@
+<?php 
+
+$var1 = "virensys";
+$var2 = "curso";
+$var3 = "2021";
+
+
+
+echo "<div><p>hola mundo !!!  ".$var1." ".$var2." ".$var3."</p></div>";
+
+?>
+
+<div>Misrecetas.com © <?php echo date("Y"); ?></div>
+
